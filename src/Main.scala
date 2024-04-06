bc0510f0428a77a15c696686015d68a7a123f52c
PedroTorrado <pedro.espanha.torrado@gmail.com>
2024-04-06 17:00:32 +0100

    Updated methods Main, randomChar Added MyRandom

--- src/Main.scala
+++ src/Main.scala
@@ -2,7 +2,14 @@ import scala.util.Random
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val (randomLetter) = Tabuleiro.randomChar(Random) // Chamar randomchar com a instância de MyRandom
+    val seed = 0// Use o tempo atual como semente para garantir aleatoriedade
+
+    val rand = Tabuleiro.MyRandom(seed) // Criar uma instância de MyRandom
+
+    val (randomLetter, newRand) = Tabuleiro.randomChar(rand) // Chamar randomChar com a instância de MyRandom
     println(s"Letra aleatória: $randomLetter")
+
+    val (randomLetter2, _) = Tabuleiro.randomChar(newRand)
+    println(s"Outra letra aleatória: $randomLetter2")
   }
 }
--- src/Tabuleiro.scala
+++ src/Tabuleiro.scala
@@ -2,11 +2,21 @@ import scala.util.Random
 
 object Tabuleiro {
 
-  def randomChar(r: Random): Char = {
-    val i = r.nextInt(26)
-    val randomChar = ('A'.toInt + i).toChar
-    (randomChar)
+  import scala.util.Random
+
+  case class MyRandom(seed: Long) {
+    def nextInt: (Int, MyRandom) = {
+      val random = new Random(seed)
+      val randomNumber = random.nextInt(26)
+      val newSeed = random.nextLong()
+      val nextRandom = MyRandom(newSeed)
+      (randomNumber, nextRandom)
+    }
   }
 
-
+  def randomChar(rand: MyRandom): (Char, MyRandom) = {
+    val (randomInt, nextRand) = rand.nextInt
+    val randomChar = ('A'.toInt + randomInt).toChar
+    (randomChar, nextRand)
+  }
 }
