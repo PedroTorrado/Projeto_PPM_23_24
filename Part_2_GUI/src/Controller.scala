981efcde35bf0f868175f9b4ec61848e916e8bb9
PedroTorrado <pedro.espanha.torrado@gmail.com>
2024-04-30 13:56:06 +0100

    Added base for GUI project

---
+++ Part_2_GUI/src/Controller.scala
@@ -0,0 +1,11 @@
+import javafx.fxml.{FXML, FXMLLoader}
+import javafx.scene.{Parent, Scene}
+import javafx.scene.layout.GridPane
+import javafx.stage.Stage
+
+class Controller {
+
+  @FXML
+  private var gridPane: GridPane = _
+
+}
